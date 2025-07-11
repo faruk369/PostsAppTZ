4f7f6d11ae81b88ae9bcce297f112715147c3552
Umar Faruk Nasir <86917307+faruk369@users.noreply.github.com>
2025-07-11 15:22:22 +0300

    Initial Commit

---
+++ PostsAppTZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostsAppTZ
+//
+//  Created by Faryk on 11.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
